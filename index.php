6bcfa3ea3de80c878721303200d24b80c93cda5f
TheTCore <le3tcore@gmail.com>
2015-02-11 10:24:13 +0100

    Megoldás

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Üzenet</h1>
+        <form method="post" action="uzenet.php">
+            <p>Név: <input type="text" name="nev">
+            <p>Telefon: <input type="text" name="telefon">
+            <p>e-mail: <input type="text" name="email">
+            <p>Üzenet: <input type="text" name="uzenet">
+            <p><input type="submit">
+        </form>
+    </body>
+</html>
---
+++ uzenet.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Üzenet érkezett:</h1>
+        <p> Név: <?php echo $_POST["nev"];?>
+        <p> Tel.: <?php echo $_POST["telefon"];?>
+        <p> e-mail: <?php echo $_POST["email"];?>
+        <p> üzenet: <?php echo $_POST["uzenet"];?>
+            
+            
+        <p><a href="index.php">Vissza</a>
+    </body>
+</html>
+
